ba61b5b112e3b20a4688a538862a64dff8406a12
EliMounjim <93162668+EliMounjim@users.noreply.github.com>
2022-04-19 23:44:47 -0600

    sixth commit

--- Movies.js
+++ Movies.js
@@ -19,7 +19,7 @@ var MovieSchema = new Schema({
     YearReleased: {type: Number, required: true, trim: true},
     Genre: {type: String, required: true, trim: true},
     Actors:  {type: Array, default: []},
-    ImageUrl: {type: String, required: [true, 'Missing image url'], unique: true}
+    ImageUrl: {type: String, required: [true, 'Missing image url']}
 });
 
 
