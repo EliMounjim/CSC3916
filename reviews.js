df6973aeee0cd65a20d3facf873f99d5bd3f3435
EliMounjim <93162668+EliMounjim@users.noreply.github.com>
2022-04-19 22:39:33 -0600

    sixth commit

--- reviews.js
+++ reviews.js
@@ -4,30 +4,11 @@ var Schema = mongoose.Schema;
 mongoose.Promise = global.Promise;
 
 var ReviewSchema = new Schema({
-    ReviewerName: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    Quote: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    Rating: {
-        type: Number,
-        required: true,
-        trim: true
-    },
-    MovieName: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    Comments: {
-        type: [ { comentatorName: String, comment: String } ],
-        required: true
-    }
+    ReviewerName: { type: String, required: true, trim: true},
+    Quote: { type: String, required: true, trim: true},
+    rating: { type: Number, enum: [0,  1,  2,  3,  4,  5], required: [true, 'missing rating'] },
+    MovieName: { type: String, required: true, trim: true},
+    Comments: { type: [ { comentatorName: String, comment: String } ], required: [true, 'missing comment']}
 });
 
 
